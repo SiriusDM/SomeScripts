699c2f4fab155e6da1a369d26a6989e30f43a376
SiriusDM <374462522@qq.com>
2023-07-19 16:54:51 +0800

    [update] add huggingface access token

--- download_scripts/download_huggingface.py
+++ download_scripts/download_huggingface.py
@@ -2,13 +2,14 @@ from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
 import datasets
 
-prefix = 'file path prefix'
-model_name = 'model name'
+prefix = ''
+model_name = ''
+access_token = ''
 
 def download_dataset():
-    dataset_name = 'dataset name'
+    dataset_name = 'imagenet-1k'
     file_path = prefix + dataset_name
-    dataset = datasets.load_dataset("dataset name")
+    dataset = datasets.load_dataset(dataset_name, split=[''],use_auth_token=access_token, cache_dir='')
     dataset.save_to_disk(file_path)
 
 def download_model():
@@ -18,10 +19,10 @@ def download_model():
 
 def download_tokenizer():
     file_path = prefix + model_name + '-tokenizer'
-    tokenizer = AutoTokenizer.from_pretrained('t5-small')
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.save_pretrained(file_path)
 
 if __name__ == "__main__":
     # download_dataset()
     # download_model()
-    download_tokenizer()
+    # download_tokenizer()
